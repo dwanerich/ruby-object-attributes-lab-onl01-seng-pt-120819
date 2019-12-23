9a82d4e217607bcdc1dc57ef4d817e172c26cff2
dwane richards <dwanerich@gmail.com>
2019-12-23 03:01:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,41 @@
 class Dog
  
- 
+   def initialize(name)
+   @name = name
+   end
+   
+   def name=(name)
+     name = name
+     @name = name
+   end
+   
+   def breed =(breed)
+     breed = breed
+     @breed = breed
+   end
+     
  
 end
 
 
-
+class Person
+ 
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+ 
+  def name=(full_name)
+    first_name, last_name = full_name.split
+    @first_name = first_name
+    @last_name = last_name
+  end
+ 
+  def name
+    "#{@first_name} #{@last_name}".strip
+  end
+ 
+end
 
 
 
